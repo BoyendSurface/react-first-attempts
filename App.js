6a0f58de5182722db480475490386279370f4df1
Ayman Regr <100210056+BoyendSurface@users.noreply.github.com>
2022-06-15 00:29:41 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <header className="App"> 
+      <nav id="nav-bar">
+        <img src="./images/icon-react.png" alt="logo"
+         className="--nav" />
+        <h3 className="nav-title">ReactFacts</h3>
+        <h4 className='nav-course'>React Course - Project 1</h4>
+      </nav>
+    </header>
+  );
+}
+
+export default App;
